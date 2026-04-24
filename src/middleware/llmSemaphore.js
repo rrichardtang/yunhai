ac1fc1126bed8f93c7be798a5465bbde5d26a955
Richard Tang <rkchardtang@gmail.com>
2026-04-24 21:55:01 +0200

    refactor: extract llmSemaphore middleware from server.js

---
+++ src/middleware/llmSemaphore.js
@@ -0,0 +1,22 @@
+const MAX_CONCURRENT_LLM_CALLS = 10;
+let activeLlmCalls = 0;
+const llmQueue = [];
+
+const acquire = () => new Promise((resolve) => {
+  const tryAcquire = () => {
+    if (activeLlmCalls < MAX_CONCURRENT_LLM_CALLS) {
+      activeLlmCalls++;
+      resolve();
+    } else {
+      llmQueue.push(tryAcquire);
+    }
+  };
+  tryAcquire();
+});
+
+const release = () => {
+  activeLlmCalls--;
+  if (llmQueue.length > 0) llmQueue.shift()();
+};
+
+module.exports = { acquire, release, MAX_CONCURRENT_LLM_CALLS };
